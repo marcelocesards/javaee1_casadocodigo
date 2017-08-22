81e5b912bef89d36733fb400f1b21ed84312a8d6
marce <marce@DESKTOP-BHMQF5G.home>
2017-08-21 22:31:54 -0300

    Finalizada a aula 1 - 08 Criando nosso primeiro formulário

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,10 +1,12 @@
 package br.com.casadocodigo.loja.beans;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
 import br.com.casadocodigo.loja.models.Livro;
 
 @Named
+@RequestScoped
 public class AdminLivrosBean {
 
 	private Livro livro = new Livro();
